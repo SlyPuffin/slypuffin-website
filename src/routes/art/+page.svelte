47aa02c09de044b2c48081dd122bcdc0255ac548
SlyPuffin <daniel@nttsmc.com>
2023-03-10 13:52:26 -0800

    Add a visualization created with bpy (python+blender)

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,5 +1,16 @@
+<style>
+  video {
+    width: 20%;
+    height: auto;
+  }
+</style>
+
 <main>
   <h1>Art</h1>
-  <p>List of projects will go here</p>
+  <p>Projects:</p>
+
+  <video controls autoplay loop muted>
+    <source src="Visualization_01_Wiggling.mp4" type="video/mp4">
+  </video>
 </main>
 
